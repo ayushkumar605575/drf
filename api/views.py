d39b3f5d3df9c5b036e1f90b77dc76d069041f89
ayush <ayushkumar605575@gmail.com>
2024-04-14 21:15:49 +0530

    DRF

--- api/views.py
+++ api/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from django.http import JsonResponse
+from products.models import Product
 # Create your views here.
+def api_view(request, *args, **kwargs):
+    model_data = Product.objects.all().order_by('?').first()
+    data = {}
+    if model_data:
+        data = {
+            'id': model_data.id,
+            'name': model_data.title,
+            'content': model_data.content,
+            'price': model_data.price,
+        }
+    return JsonResponse(data)
