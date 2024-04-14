31db6de83f2c577036903f5b575bc7b46afc3d5a
ayush <ayushkumar605575@gmail.com>
2024-04-14 21:38:36 +0530

    DRF

--- api/views.py
+++ api/views.py
@@ -1,15 +1,11 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from products.models import Product
+from django.forms.models import model_to_dict
 # Create your views here.
 def api_view(request, *args, **kwargs):
     model_data = Product.objects.all().order_by('?').first()
     data = {}
     if model_data:
-        data = {
-            'id': model_data.id,
-            'name': model_data.title,
-            'content': model_data.content,
-            'price': model_data.price,
-        }
+        data = model_to_dict(model_data, fields=['id', 'title'])
     return JsonResponse(data)
